617498322bd2061fabee6112b762010792a3b676
depstor <depstor90@gmail.com>
2020-09-28 00:46:45 +0300

    validateV2

--- script/script.js
+++ script/script.js
@@ -5,13 +5,63 @@ window.addEventListener('DOMContentLoaded', () => {
         input = document.querySelectorAll('input'),
         text = document.querySelector('#text');
 
+    const submit = () => {
+        console.log('su')
+    }
+
+    let isValidate = false;
+
+    const __RegExpNumberCard = /^[0-9]+$/;
+    const __RegExpName = /^[a-z0-9_-]{4,20}$/;
+
     const validateElem = (elem) => {
-        console.log(elem)
+        if (elem.name === 'numberCard') {
+            if (!__RegExpNumberCard.test(elem.value) && elem.value !== '') {
+                text.textContent =
+                    "Введите корректные данные";
+                isValidate = false;
+                elem.classList.add('error');
+            } else {
+                text.textContent = "";
+                isValidate = true;
+                elem.classList.remove('error');
+            }
+        }
+
+        if (elem.name === 'userName') {
+            if (!__RegExpName.test(elem.value) && elem.value !== '') {
+                elem.nextElementSibling.textContent =
+                    "Введите корректное имя пользователя";
+                isValidate = false;
+                elem.classList.add('error');
+            } else {
+                elem.nextElementSibling.textContent = "";
+                isValidate = true;
+                elem.classList.remove('error');
+            }
+        }
+
+
+
+        if (elem.name === 'cvc') {
+            if (!__RegExpNumberCard.test(elem.value) && elem.value !== '') {
+                elem.nextElementSibling.textContent =
+                    "Введите корректные данные";
+                isValidate = false;
+                elem.classList.add('error');
+            } else {
+                elem.nextElementSibling.textContent = "";
+                isValidate = true;
+                elem.classList.remove('error');
+            }
+        }
+
+
     }
 
     input.forEach(elem => {
         elem.addEventListener('blur', () => {
-            validateElem(elem)
+            validateElem(elem);
         })
     })
 
@@ -21,12 +71,22 @@ window.addEventListener('DOMContentLoaded', () => {
         input.forEach(elem => {
             if (elem.value === '') {
                 text.textContent = "Заолните все поля";
+                isValidate = false;
                 elem.classList.add('error');
             } else {
                 text.textContent = "";
                 elem.classList.remove('error');
+                isValidate = true;
             }
         })
+
+        if (isValidate) {
+            submit();
+
+            form.reset();
+        } else {
+            console.log('ggg')
+        }
     })
 
     // function validate() {
@@ -80,8 +140,8 @@ window.addEventListener('DOMContentLoaded', () => {
     // }
 
     function burgerMenu() {
-        const icon = document.querySelector('.icon');
-        const link = document.querySelector('.menu__link');
+        const icon = document.querySelector('.icon'),
+            link = document.querySelector('.menu__link');
 
         icon.addEventListener('click', () => {
             icon.classList.toggle('active');
@@ -97,4 +157,5 @@ window.addEventListener('DOMContentLoaded', () => {
 
     burgerMenu();
     // validate();
+
 });
